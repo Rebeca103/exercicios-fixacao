a0d883bfc2e62b339ff7c6aa8c1f864df274c8b8
Rebeca103 <rebeca55857@estudante.ifb.edu.br>
2026-07-22 20:29:49 +0000

    feat: cria classe Conversor com métodos de conversão e testes

---
+++ orientacao_objetos/main.js
@@ -0,0 +1,7 @@
+import { Conversor } from './conversor.js';
+
+const conversor = new Conversor();
+
+console.log(`25°C = ${conversor.celsiusParaFahrenheit(25)}°F`);
+console.log(`10 km = ${conversor.quilometrosParaMilhas(10)} milhas`);
+console.log(`150 minutos = ${conversor.minutosParaHoras(150)} horas`);
